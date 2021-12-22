cfff144afb33ddf9c9f04f3e0732eb61065d2926
Luciano Scialchi <luccianno.scialchi2012@gmail.com>
2021-12-22 13:26:16 -0300

    seguimos con las pruebas
    f

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # hello-world
 Saludar
 estes es  mi saludo 
+
+a rchivo1=open('cajeros.txt','r') 
--- prueba_2.py
+++ prueba_2.py
@@ -1,2 +1,14 @@
-#vveefe
-FileExistsError
+
+
+
+archivo1=open('IDEC.txt','r') 
+    
+cont = 1
+linea1=archivo1.readline().strip()
+i = 0
+while linea1 != '':
+        
+    print((linea1.split(',')[0]))
+    cont = cont  + 1
+    i = i + 1
+    linea1=archivo1.readline().strip()
